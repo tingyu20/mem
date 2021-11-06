1627849ddddefe1399d50ea69233d1faeb8d5de0
che <2252310874@qq.com>
2021-11-06 12:45:42 +0800

    框架

---
+++ src/test/java/com/cumt/mem/MemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumt.mem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
